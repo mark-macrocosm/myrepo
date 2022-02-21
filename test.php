52cabc14d46597c99cff55dd6e09207698186423
Mark Jivko <99642656+mark-macrocosm@users.noreply.github.com>
2022-02-21 09:15:58 +0200

    Update test.php

--- test.php
+++ test.php
@@ -10,7 +10,54 @@
  */
 class Singleton {
     
-    // @TODO Implement Singleton functionality
+    /**
+     * Singleton instance
+     * 
+     * @var Singleton
+     */
+    protected static $_instance = null;
+
+    /**
+     * Use constants for immutable types instead of variables
+     * Use descriptive names
+     */
+    const STRING_A      = 'A';
+    const STRING_A_LONG = 'stringA';
+    const STRING_B_LONG = 'stringB';
+    const STRING_NON_A  = '^A';
+    const STRING_NON_B  = '^B';
+    const STRING_NON_C  = '^C';
+    const STRING_ABC    = 'ABC';
+    const INT_A         = 1;
+    const INT_B         = 2;
+
+    /**
+     * Directory where users have read-only access to certain file types
+     */
+    const PATH_FILES = '/real/path/to/files';
+
+    /**
+     * Get a singleton instance
+     * 
+     * @return Singleton
+     */
+    public static function getInstance() {
+        if (null === self::$_instance) {
+            self::$_instance = new self();
+        }
+
+        return self::$_instance;
+    }
+
+    /**
+     * Inaccessible constructor
+     */
+    protected function __construct () {}
+
+    /**
+     * Prevents this class from being cloned
+     */
+    protected function __clone() {}
     
     /**
      * Display user name
@@ -18,11 +65,14 @@ class Singleton {
      * @param string $name User-provided name
      */
     public function userEcho($name) {
-        // @TODO Validate & sanitize $name
-        $val_san_name = filter_var($name, FILTER_SANITIZE_STRING);
-        echo "The value of 'name' is '{$val_san_name}'";
+        // Please check PSR-2 specification; variable names are cammel-cased
+        # $val_san_name = filter_var($name, FILTER_SANITIZE_STRING);
+        
+        // Prevent XSS
+        $valSanName = filter_var($name, FILTER_SANITIZE_STRING);
+        
+        echo "The value of 'name' is '{$valSanName}'";
     }
-
     
     /**
      * Query by user name
@@ -30,19 +80,39 @@ class Singleton {
      * @param string $name User-provided name
      */
     public function userQuery($name) {
-        // @TODO Validate & sanitize $name
-        $val_san_name = filter_var($name, FILTER_SANITIZE_STRING);
-        mysql_query("SELECT * FROM `test` WHERE `name` = '{$val_san_name}' LIMIT 1");
+        // Does not prevent SQL injections
+        // $val_san_name = filter_var($name, FILTER_SANITIZE_STRING);
+        
+        // Escape the string or use a prepared statement
+        $name = mysql_real_escape_string($name);
+        mysql_query("SELECT * FROM `test` WHERE `name` = '{$name}' LIMIT 1");
     }
+    
     /**
      * Output the contents of a file
      * 
      * @param string $path User-provided file path
      */
     public function userFile($path) {
-        // @TODO Validate & sanitize $path
-        $val_san_path = filter_var($path, FILTER_SANITIZE_URL);
-        readfile($val_san_path);
+        // Paths are not URLs
+        # $val_san_path = filter_var($path, FILTER_SANITIZE_URL);
+        
+        // User paths are relative to this root
+        $root = self::PATH_FILES;
+
+        // The main point is to never allow users to perform directory traversal
+        // Special characters like "." and ".." and direct root access should be forbidden
+        // Validate relative path, file name and extension
+        if (!preg_match('%^(?:allowed_subpath_a|allowed_subpath_b)\/\w+\.(?:ext|png|jpe?g)$%i', $path)) {
+            throw new Exception('Invalid file path');
+        }
+
+        // File not found; also check that it's a file, not a directory
+        if (!is_file("$root/$path")) {
+            throw new Exception('File not found');
+        }
+
+        readfile("$root/$path");
     }
     
     /**
@@ -50,54 +120,65 @@ class Singleton {
      */
 
     public function nestedConditionsup() {
-        // @TODO Untangle nested conditions
-        if(!$conditionA)
-        {
-            echo '^A'; 
-            return;
-        }
-        if (!$conditionB) {
-            echo '^B';
-            return;
-        }
-        if (!$conditionC) {
-            echo '^C';
-            return;
-        }
-        echo 'ABC';
-        return;
+        // Don't introduce new constants
+        // The do {} while(false) technique avoids multiple returns
+        do {
+            if (!$conditionA) {
+                echo self::STRING_NON_A;
+                break;
+            }
+
+            if (!$conditionB) {
+                echo self::STRING_NON_B;
+                break;
+            }
+
+            if (!$conditionC) {
+                echo self::STRING_NON_C;
+                break;
+            }
+
+            echo self::STRING_ABC;
+        } while(false);
     } 
+    
     /**
      * Return statements
      * 
      * @return boolean
      */
     public function returnStatements($conditionA) {
-        // @TODO Fix
+        // One return per function
         if ($conditionA) {
-            echo 'A';
-            return true;
-        } 
-        return false;
+            echo self::STRING_A;
+        }
+
+        // Implicit boolean conversion
+        return !!$conditionA;
     }
 
-
+    /**
+     * Null coalescing
+     */
     public function nullCoalescing() {
-        // @TODO Simplify
-       
-        $name=isset($_GET['name']) ? $_GET['name'] : isset($_POST['name']) ? $_POST['name'] : 'nobody';
-        return $name;
+        return $_GET['name'] ?? $_POST['name'] ?? 'nobody';
     }
     
     /**
      * Method chaining
      */
     public function methodChained() {
-        // @TODO Implement method chaining
-        $singleton = (new Singleton)
-            ->returnStatements($conditionA)
-            ->userQuery("Atta")
-            ->userFile($path);
+        // You cannot call new for a Singleton class
+        # $singleton = (new Singleton)
+        
+        // The function returns a boolean, not an instance of Singleton
+        #    ->returnStatements($conditionA)
+        
+        // The function returns void, not an instance of Singleton
+        #    ->userQuery("Atta")
+        #    ->userFile($path);
+        
+        return $this;
     }
     
     /**
@@ -105,19 +186,23 @@ class Singleton {
      */
     public function checkValue($value) {
         $result = null;
-        $singleton = new Singleton;
-        // @TODO Make all the immutable values (int, string) in this class 
-        // easily replaceable
-        //  im quite confuse in it and thats why i just wrote the algo 
+        // You cannot call new for a Singleton class
+        # $singleton = new Singleton;
+
+        // We should't use constants (strings, ints) locally
+        // Store them as class constants instead
         switch ($value) {
-            case is_int($value):
-                $singleton->name=$value; 
+            case self::STRING_A_LONG:
+                $result = INT_A;
                 break;
-            case is_string($value):
-                $singleton->name=$value; 
+
+            case self::STRING_B_LONG:
+                $result = INT_B;
                 break;
+
+            // The default is already set, its' null
         }
-        
+
         return $result;
     }
     
@@ -128,11 +213,12 @@ class Singleton {
      * @return boolean
      */
     public function regexTest($time24Hour) {
-
-        return preg_match('/^([0-1]?[0-9]|[2][0-3]):([0-5][0-9])(:[0-5][0-9])?$/', $time24Hour);
-
+        // 0 left padding - so [01]\d instad of [01]?\d (02:00 instead of 2:00)
+        // DRY - don't repeat yourself, the 00-59 minute/second block can appear once or twice
+        // Don't use capturing blocks if you don't need them - (?:) instead of ()
+        // preg_match returns 0,1 or false; expected return value is boolean
+        return !!preg_match('#^(?:[01]\d|2[0-3])(?:\:[0-5]\d){1,2}$#', $time24Hour);
     }
-    
 }
 
 /*EOF*/
